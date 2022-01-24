1d7fb55d6dda7571a7334edb8119bd5b51d01d76
luhcs <lucas_fgomes@CAMP-CMP-01.localdomain>
2022-01-24 15:01:05 -0300

    try2

--- main.js
+++ main.js
@@ -20,21 +20,21 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 // Texturas Cerebro
-var normalTexture = new THREE.TextureLoader().load('./test_Normal.png');
+var normalTexture = new THREE.TextureLoader().load('./assets/test_Normal.png');
 normalTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
 normalTexture.flipY = false;
 
-var aoOcclusion = new THREE.TextureLoader().load('./test_Occlusion.png');
+var aoOcclusion = new THREE.TextureLoader().load('./assets/test_Occlusion.png');
 aoOcclusion.anisotropy = renderer.capabilities.getMaxAnisotropy();
 aoOcclusion.flipY = false;
-var aoMetalness = new THREE.TextureLoader().load('./test_Metalness.png');
+var aoMetalness = new THREE.TextureLoader().load('./assets/test_Metalness.png');
 aoMetalness.anisotropy = renderer.capabilities.getMaxAnisotropy();
 aoMetalness.flipY = false;
-var aoGloss = new THREE.TextureLoader().load('./test_Gloss.png');
+var aoGloss = new THREE.TextureLoader().load('./assets/test_Gloss.png');
 aoGloss.anisotropy = renderer.capabilities.getMaxAnisotropy();
 aoGloss.flipY = false;
-var map = new THREE.TextureLoader().load('./test_Albedo.png');
+var map = new THREE.TextureLoader().load('./assets/test_Albedo.png');
 map.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
 map.flipY = false;
@@ -45,7 +45,7 @@ map.flipY = false;
 
 let brain;
 const loader = new THREE.GLTFLoader();
-loader.load('./scene.gltf', 
+loader.load('./models/scene.gltf', 
 function (gltf) {
   brain = gltf.scene.children[0];
   brain.material = new THREE.MeshStandardMaterial({
@@ -111,7 +111,7 @@ const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('./space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('./assets/space.jpg');
 scene.background = spaceTexture;
 
 // Animação
